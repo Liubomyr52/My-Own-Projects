186bef9f47443747325506603c5e1745b9939d3a
Liubomyr H <liubomirguzhumit@gmail.com>
2025-10-22 21:17:03 -0400

    new commits

---
+++ script.js
@@ -0,0 +1,72 @@
+var name = "John";
+console.log(name);
+
+
+
+
+
+
+
+let user = 'John', age = 25, message = 'Hello';
+console.log(user, age, message);
+
+
+
+
+
+
+
+let text;
+
+text = 'Hello';
+text = 'World!';
+alert(text);
+
+
+
+
+
+
+
+let name1 = 'Liubomyr';
+let age1 = 13;
+
+alert(name1 + age1);
+
+
+
+
+
+
+
+
+let name2 = 'Liubomyr';
+
+name2 = "Alex"
+
+console.log(name2);
+
+
+
+
+
+
+
+//let let = 'Liubomyr'   SYNTAX ERROR
+//let class = 'Alex;     SYNTAX ERROR
+//let return = 'John'    Syntax ERROR
+//let function = 'Alice' SYNTAX ERROR
+
+
+
+
+
+
+let age3 = 30;
+
+if (true) {
+    let age3 = 25;
+    console.log(age3);
+}
+
+console.log(age3);
